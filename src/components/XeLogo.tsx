0d5ca1ccbd8708850fcf0cb12705c68b99cfec51
RAJ <rajshrivastav283815@gmail.com>
2026-06-25 03:09:38 +0530

    updated logo

--- src/components/XeLogo.tsx
+++ src/components/XeLogo.tsx
@@ -3,12 +3,15 @@ import { SVGProps } from "react";
 export function XeLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg 
-      viewBox="0 0 140 60" 
+      viewBox="0 0 90 40" 
       fill="currentColor" 
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path d="M 0 0 L 20 0 L 40 20 L 60 0 L 140 0 L 140 14 L 66 14 L 57 23 L 130 23 L 130 37 L 57 37 L 66 46 L 140 46 L 140 60 L 60 60 L 40 40 L 20 60 L 0 60 L 30 30 Z" />
+      <path d="M 0 0 H 30 L 50 20 L 30 40 H 0 L 20 20 Z" />
+      <path d="M 40 0 H 90 V 10 H 50 Z" />
+      <path d="M 50 30 H 90 V 40 H 40 Z" />
+      <path d="M 55 15 L 60 20 L 55 25 H 75 V 15 Z" />
     </svg>
   );
 }
